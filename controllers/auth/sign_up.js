4b32f9f94dd74d492cd2107a4dae4afc24a29795
Scott Bowler <scott.bowler@learningfuze.com>
2019-01-09 13:15:50 -0800

    Initial user sign up working

---
+++ controllers/auth/sign_up.js
@@ -0,0 +1,67 @@
+const { users } = require('../../db/models');
+const validation = require('../../helpers/validation');
+
+module.exports = async (req, res) => {
+    const { body: { firstName, lastName, email, password } } = req;
+
+    try {
+        const errors = [];
+
+        if(!firstName){
+            errors.push('Missing first name');
+        } else if(!validation.name(firstName)){
+            errors.push('First name can only contain a-z, no special characters allowed');
+        }
+
+        if (!lastName) {
+            errors.push('Missing last name');
+        } else if (!validation.name(lastName)) {
+            errors.push('Last name can only contain a-z, no special characters allowed');
+        }
+
+        if(!email){
+            errors.push('Missing email address');
+        } else if(!validation.email(email)){
+            errors.push('Invalid email address given. Must be in following format: example@mail.com');
+        }
+
+        if(!password){
+            errors.push('Missing password');
+        } else if(!validation.password(password)){
+            errors.push('Invalid password. Must contain upper and lower case letters, numbers, special characters, and be at least 8 characters long');
+        }
+
+        if(errors.length){
+            return res.status(422).send({
+                success: false,
+                errors
+            });
+        }
+
+        const existingUser = await users.findOne({
+            where: { email }
+        });
+        
+        if(existingUser){
+            return res.status(422).send('User already exists');
+        }
+
+        const newUser = users.build({
+            email,
+            firstName,
+            lastName,
+            password
+        });
+
+        await newUser.save();
+
+        res.send({
+            success: true,
+            message: 'This is the sign up endpoint'
+        });
+    } catch(err){
+        console.log('Sign Up Error:', err);
+
+        res.status(500).send('Sign Up Failed');
+    }
+}
